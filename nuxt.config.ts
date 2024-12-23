cb5c1799468952865030864c4d62f05b0bdac811
yvzl <898101408@qq.com>
2024-12-23 18:57:21 +0800

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   typescript: {
     typeCheck: true,
     strict: true
-  }
+  },
+  modules: ['@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt'],
 })
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: ['auth']
+})
+</script>
+
+<template>
+<p>111</p>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
